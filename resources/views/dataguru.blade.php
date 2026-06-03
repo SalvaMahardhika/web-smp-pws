edc8d0e8f4df814ffa47f0eea4345655b1be0ee3
SalvaMahardhika <yoi198808@gmail.com>
2026-06-03 19:15:27 +0700

    revisi dropdown guru1

--- resources/views/dataguru.blade.php
+++ resources/views/dataguru.blade.php
@@ -122,6 +122,8 @@
                             <option value="Seni Budaya">Seni Budaya</option>
                             <option value="Teknologi Informasi dan Komunikasi (TIK)">TIK</option>
                             <option value="Prakarya">Prakarya</option>
+                            <option value="Bimbingan Konseling (BK)">Bimbingan Konseling (BK)</option>
+                            <option value="Bahasa Jawa">Bahasa Jawa</option>
                         </select>
                         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 text-slate-800">
                             <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -167,6 +169,8 @@
                             <option value="Seni Budaya">Seni Budaya</option>
                             <option value="Teknologi Informasi dan Komunikasi (TIK)">TIK</option>
                             <option value="Prakarya">Prakarya</option>
+                            <option value="Bimbingan Konseling (BK)">Bimbingan Konseling (BK)</option>
+                            <option value="Bahasa Jawa">Bahasa Jawa</option>
                         </select>
                         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 text-slate-800">
                             <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
